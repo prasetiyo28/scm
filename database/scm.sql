8a6f9f62d6244b284bbe1da8bed1838e715a021c
Bayu Adi Prasetiyo <bayu28.bap@gmail.com>
2019-07-02 07:01:57 +0700

    db update july

--- database/scm.sql
+++ database/scm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.6
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 23, 2019 at 04:49 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Generation Time: Jul 02, 2019 at 02:00 AM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 7.2.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -41,7 +41,9 @@ CREATE TABLE `cabang` (
 --
 
 INSERT INTO `cabang` (`id_cabang`, `id_user`, `nama_cabang`, `alamat`, `no_telp`) VALUES
-(1, 1, 'Cabang Adiwerna', 'Adiwerna', '082345697');
+(1, 1, 'Cabang Adiwerna', 'Jl. Raya tegal slawi Desa tembok luwung kec. Adiwerna', '0828129128'),
+(2, 3, 'Cabang Debong', 'Jalan Raya Debong', '0283282819'),
+(3, 4, 'Cabang Talang', 'Jalan Raya Talang No.30', '0891212121');
 
 -- --------------------------------------------------------
 
@@ -53,8 +55,8 @@ CREATE TABLE `pengeluaran` (
   `id_pengeluaran` int(11) NOT NULL,
   `id_cabang` int(11) NOT NULL,
   `jumlah` int(5) NOT NULL,
-  `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` tinyint(1) NOT NULL DEFAULT '0'
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -62,7 +64,9 @@ CREATE TABLE `pengeluaran` (
 --
 
 INSERT INTO `pengeluaran` (`id_pengeluaran`, `id_cabang`, `jumlah`, `tanggal`, `status`) VALUES
-(1, 1, 50, '2019-05-23 05:47:07', 0);
+(1, 1, 50, '2019-05-23 05:47:07', 0),
+(5, 1, 20, '2019-07-01 21:39:46', 0),
+(6, 1, 20, '2019-07-01 22:06:09', 0);
 
 -- --------------------------------------------------------
 
@@ -74,8 +78,8 @@ CREATE TABLE `stock` (
   `id_stock` int(11) NOT NULL,
   `id_cabang` int(11) NOT NULL,
   `jumlah` int(5) NOT NULL,
-  `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` tinyint(1) NOT NULL DEFAULT '0'
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -84,7 +88,8 @@ CREATE TABLE `stock` (
 
 INSERT INTO `stock` (`id_stock`, `id_cabang`, `jumlah`, `tanggal`, `status`) VALUES
 (1, 1, 23, '2019-05-23 05:45:49', 3),
-(2, 1, 150, '2019-05-23 05:46:52', 3);
+(2, 1, 150, '2019-05-23 05:46:52', 3),
+(3, 1, 20, '2019-07-01 22:40:09', 3);
 
 -- --------------------------------------------------------
 
@@ -98,8 +103,8 @@ CREATE TABLE `user` (
   `email` varchar(150) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `jenis_user` tinyint(1) NOT NULL DEFAULT '0',
-  `deleted` tinyint(1) NOT NULL DEFAULT '0'
+  `jenis_user` tinyint(1) NOT NULL DEFAULT 0,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -108,7 +113,9 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id_user`, `nama`, `email`, `username`, `password`, `jenis_user`, `deleted`) VALUES
 (1, 'Bunga', 'bunga@gmail.com', 'bunga', '80219675a4b4f2bb1fa1e48fe8397f30', 1, 0),
-(2, 'lestari', 'lestari@chicken.com', 'lestari', 'f3bab21fe648634117ba2e1d70b09740', 2, 0);
+(2, 'lestari', 'lestari@chicken.com', 'lestari', 'f3bab21fe648634117ba2e1d70b09740', 2, 0),
+(3, 'danny', 'danny@gmail.com', 'danny', 'b7bee6b36bd35b773132d4e3a74c2bb5', 1, 0),
+(4, 'Awkarin', 'awkarin@gmail.com', 'awkarin', '4a4980e406461ec6039edc2eca71b392', 1, 0);
 
 --
 -- Indexes for dumped tables
@@ -146,25 +153,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `cabang`
 --
 ALTER TABLE `cabang`
-  MODIFY `id_cabang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_cabang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `pengeluaran`
 --
 ALTER TABLE `pengeluaran`
-  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `stock`
 --
 ALTER TABLE `stock`
-  MODIFY `id_stock` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_stock` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
