dba836c9df23b830cd3616de14791eaae5db56e0
Bayu Adi Prasetiyo <bayu28.bap@gmail.com>
2019-07-17 15:55:44 +0700

    update db

--- database/scm.sql
+++ database/scm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 02, 2019 at 02:00 AM
+-- Generation Time: Jul 17, 2019 at 10:54 AM
 -- Server version: 10.3.15-MariaDB
 -- PHP Version: 7.2.19
 
@@ -42,8 +42,69 @@ CREATE TABLE `cabang` (
 
 INSERT INTO `cabang` (`id_cabang`, `id_user`, `nama_cabang`, `alamat`, `no_telp`) VALUES
 (1, 1, 'Cabang Adiwerna', 'Jl. Raya tegal slawi Desa tembok luwung kec. Adiwerna', '0828129128'),
-(2, 3, 'Cabang Debong', 'Jalan Raya Debong', '0283282819'),
+(2, 3, 'Cabang Debong', 'Jalan Raya Debong Raya', '08752322476'),
 (3, 4, 'Cabang Talang', 'Jalan Raya Talang No.30', '0891212121');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kasir`
+--
+
+CREATE TABLE `kasir` (
+  `id_kasir` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_cabang` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kasir`
+--
+
+INSERT INTO `kasir` (`id_kasir`, `id_user`, `id_cabang`) VALUES
+(1, 5, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `kategori` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `kategori`, `harga`) VALUES
+(1, 'Paha', 8000),
+(2, 'Dada', 10000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paket`
+--
+
+CREATE TABLE `paket` (
+  `id_paket` int(11) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `banyak` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `keterangan` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `paket`
+--
+
+INSERT INTO `paket` (`id_paket`, `nama`, `id_kategori`, `banyak`, `harga`, `keterangan`) VALUES
+(1, 'Paket Panas 1', 1, 1, 15000, 'Paha Atas 1 , Nasi 1 , Es Teh');
 
 -- --------------------------------------------------------
 
@@ -66,7 +127,8 @@ CREATE TABLE `pengeluaran` (
 INSERT INTO `pengeluaran` (`id_pengeluaran`, `id_cabang`, `jumlah`, `tanggal`, `status`) VALUES
 (1, 1, 50, '2019-05-23 05:47:07', 0),
 (5, 1, 20, '2019-07-01 21:39:46', 0),
-(6, 1, 20, '2019-07-01 22:06:09', 0);
+(6, 1, 20, '2019-07-01 22:06:09', 0),
+(7, 1, 100, '2019-07-02 07:29:09', 0);
 
 -- --------------------------------------------------------
 
@@ -79,6 +141,7 @@ CREATE TABLE `stock` (
   `id_cabang` int(11) NOT NULL,
   `jumlah` int(5) NOT NULL,
   `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id_kategori` int(11) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -86,10 +149,34 @@ CREATE TABLE `stock` (
 -- Dumping data for table `stock`
 --
 
-INSERT INTO `stock` (`id_stock`, `id_cabang`, `jumlah`, `tanggal`, `status`) VALUES
-(1, 1, 23, '2019-05-23 05:45:49', 3),
-(2, 1, 150, '2019-05-23 05:46:52', 3),
-(3, 1, 20, '2019-07-01 22:40:09', 3);
+INSERT INTO `stock` (`id_stock`, `id_cabang`, `jumlah`, `tanggal`, `id_kategori`, `status`) VALUES
+(1, 1, 23, '2019-05-23 05:45:49', 1, 3),
+(2, 1, 150, '2019-05-23 05:46:52', 2, 3),
+(3, 1, 20, '2019-07-01 22:40:09', 2, 3),
+(4, 1, 80, '2019-07-02 07:29:33', 1, 3),
+(5, 1, 100, '2019-07-17 07:06:43', 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_cabang` int(11) NOT NULL,
+  `id_paket` int(11) NOT NULL,
+  `jumlah` int(3) NOT NULL,
+  `struk` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_cabang`, `id_paket`, `jumlah`, `struk`, `tanggal`) VALUES
+(1, 1, 1, 3, 1, '2019-07-16 01:03:30');
 
 -- --------------------------------------------------------
 
@@ -115,7 +202,8 @@ INSERT INTO `user` (`id_user`, `nama`, `email`, `username`, `password`, `jenis_u
 (1, 'Bunga', 'bunga@gmail.com', 'bunga', '80219675a4b4f2bb1fa1e48fe8397f30', 1, 0),
 (2, 'lestari', 'lestari@chicken.com', 'lestari', 'f3bab21fe648634117ba2e1d70b09740', 2, 0),
 (3, 'danny', 'danny@gmail.com', 'danny', 'b7bee6b36bd35b773132d4e3a74c2bb5', 1, 0),
-(4, 'Awkarin', 'awkarin@gmail.com', 'awkarin', '4a4980e406461ec6039edc2eca71b392', 1, 0);
+(4, 'Awkarin', 'awkarin@gmail.com', 'awkarin', '4a4980e406461ec6039edc2eca71b392', 1, 0),
+(5, 'Kasir Cabang 1', 'kasir1@gmail.com', 'kasir', 'c7911af3adbd12a035b289556d96470a', 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -128,6 +216,24 @@ ALTER TABLE `cabang`
   ADD PRIMARY KEY (`id_cabang`);
 
 --
+-- Indexes for table `kasir`
+--
+ALTER TABLE `kasir`
+  ADD PRIMARY KEY (`id_kasir`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `paket`
+--
+ALTER TABLE `paket`
+  ADD PRIMARY KEY (`id_paket`);
+
+--
 -- Indexes for table `pengeluaran`
 --
 ALTER TABLE `pengeluaran`
@@ -138,6 +244,12 @@ ALTER TABLE `pengeluaran`
 --
 ALTER TABLE `stock`
   ADD PRIMARY KEY (`id_stock`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
 
 --
 -- Indexes for table `user`
@@ -156,22 +268,46 @@ ALTER TABLE `cabang`
   MODIFY `id_cabang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `kasir`
+--
+ALTER TABLE `kasir`
+  MODIFY `id_kasir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `paket`
+--
+ALTER TABLE `paket`
+  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `pengeluaran`
 --
 ALTER TABLE `pengeluaran`
-  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `stock`
 --
 ALTER TABLE `stock`
-  MODIFY `id_stock` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_stock` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
